d4d4477342409a9e77399e3c9ed8b973e20c8c7d
Eric Andrade <vloxflox@gmail.com>
2018-11-24 19:02:17 -0300

    tipos de entries adicionados

---
+++ src/insiderthreatv2/entries/HttpEntry.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insiderthreatv2.entries;
+
+/**
+ *
+ * @author eric
+ */
+public class HttpEntry extends ActivityEntry{
+    public HttpEntry(String id,String date,String user,String pc,String activity){
+        super(id,date,user,pc,activity);
+    }
+    
+}
---
+++ src/insiderthreatv2/entries/LogonEntry.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insiderthreatv2.entries;
+
+/**
+ *
+ * @author eric
+ */
+public class LogonEntry extends ActivityEntry{
+    public LogonEntry(String id,String date,String user,String pc,String activity){
+        super(id,date,user,pc,activity);
+    }
+}
